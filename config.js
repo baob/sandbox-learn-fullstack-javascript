15c89eba0445620dae3ee23e2ab6527f1dd80678
baob <coder@onesandthrees.com>
2018-12-31 15:37:18 +0000

    Revert "now export/import a function"
    This reverts commit 85300ade67babac420ca875bcdd09a9ab5b8f431.

--- config.js
+++ config.js
@@ -4,12 +4,6 @@ const env = process.env;
 
 export const nodeEnv = env.NODE_ENV || "development";
 
-export const logStars = function(message) {
-	console.log("***********");
-	console.log(message);
-	console.log("***********");
-};
-
 export default {
 	port: env.PORT || 8080
 };
