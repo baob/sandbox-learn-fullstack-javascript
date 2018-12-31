1e23ce294740db60bd2b6ffba9593f52e1099bd2
baob <coder@onesandthrees.com>
2018-12-31 11:36:50 +0000

    make an https request

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 import config from "./config";
 
 console.log(config);
+
+import https from "https"; // node core module
+
+https.get("https://www.lynda.com", res => {
+	console.log("Response status code: ", res.statusCode);
+});
