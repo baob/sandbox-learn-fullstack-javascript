af293ebfc45462b066052cb519b99bf7ceab355c
baob <coder@onesandthrees.com>
2018-12-31 14:13:36 +0000

    adding the about page, demonstrating routing

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ server.get("/", (req, res) => {
 	res.send("Hello from express !");
 });
 
+server.get("/about.html", (req, res) => {
+	res.send("The about page !");
+});
+
 server.listen(config.port, () => {
 	console.info("Express listening on port ", config.port);
 });
