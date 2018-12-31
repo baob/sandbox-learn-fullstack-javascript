b74285e52ba29507e4121bdb00f35e60d34e22ba
baob <coder@onesandthrees.com>
2018-12-31 11:20:31 +0000

    now export/import a function

--- config.js
+++ config.js
@@ -4,6 +4,12 @@ const env = process.env;
 
 export const nodeEnv = env.NODE_ENV || "development";
 
+export const logStars = function(message) {
+	console.log("***********");
+	console.log(message);
+	console.log("***********");
+};
+
 export default {
 	port: env.PORT || 8080
 };
