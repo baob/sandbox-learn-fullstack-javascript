68e87682d88dd31117b62d05f7ac8afe2bf76432
baob <coder@onesandthrees.com>
2018-12-31 11:40:37 +0000

    now listen for https data events and read the chunk buffers

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ import https from "https"; // node core module
 
 https.get("https://www.lynda.com", res => {
 	console.log("Response status code: ", res.statusCode);
+
+	res.on("data", chunk => {
+		console.log(chunk.toString());
+	});
 });
