dcec813c12c5d551d1cda9a89fb6e11232de522b
baob <coder@onesandthrees.com>
2018-12-31 13:36:05 +0000

    write to the http output stream

--- server.js
+++ server.js
@@ -9,3 +9,14 @@ const server = http.createServer();
 console.log("http server on port: ", config.port);
 
 server.listen(config.port);
+
+server.on("request", (req, res) => {
+	// logStars(req);
+	// logStars(res);
+	res.write("Hello HTTP !\n");
+
+	setTimeout(() => {
+		res.write("I can stream! \n");
+		res.end();
+	}, 3000);
+});
