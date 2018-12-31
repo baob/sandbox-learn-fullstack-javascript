db55e5979cffec2777a702e7afbd0d258ab8db3e
baob <coder@onesandthrees.com>
2018-12-31 13:56:30 +0000

    undo http server code

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
 import config from "./config";
 
 console.log(config);
-
-import http from "http";
-
-const server = http.createServer();
-
-console.log("http server on port: ", config.port);
-
-server.listen(config.port);
-
-server.on("request", (req, res) => {
-	// logStars(req);
-	// logStars(res);
-	res.write("Hello HTTP !\n");
-
-	setTimeout(() => {
-		res.write("I can stream! \n");
-		res.end();
-	}, 3000);
-});
