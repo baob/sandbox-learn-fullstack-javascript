8a3c63a4ca57806c646200a19b0a3139979e9bc7
baob <coder@onesandthrees.com>
2018-12-31 16:54:22 +0000

    ejs dynmaic content - showing escaping problem

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ import express from 'express';
 
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
 	res.render('index', {
-		content: 'Hello Express and EJS!'
+		content: 'Hello Express <em>and</em> EJS!'
 	});
 });
 
