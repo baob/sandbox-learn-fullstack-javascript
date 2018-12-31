3b5243acd7fefc747cade30e398278c05eee5d76
baob <coder@onesandthrees.com>
2018-12-31 15:52:11 +0000

    undo https changes

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
 import config from "./config";
 
 console.log(config);
-
-import https from "https"; // node core module
-
-https.get("https://www.lynda.com", res => {
-	console.log("Response status code: ", res.statusCode);
-
-	res.on("data", chunk => {
-		console.log(chunk.toString());
-	});
-});
