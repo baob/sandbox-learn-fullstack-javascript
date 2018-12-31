dd03b976cb34832f3d34e4eba26212d123f7e371
baob <coder@onesandthrees.com>
2018-12-31 13:20:24 +0000

    http server listener only - nothing done with request

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 import config from "./config";
 
 console.log(config);
+
+import http from "http";
+
+const server = http.createServer();
+
+console.log("http server on port: ", config.port);
+
+server.listen(config.port);
