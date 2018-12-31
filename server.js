7a904809b00fa74e9b24a9ca0bf262cb5da44b0e
baob <coder@onesandthrees.com>
2018-12-31 16:53:13 +0000

    ejs dynmaic content

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-import config from "./config";
-import apiRouter from "./api";
+import config from './config';
+import apiRouter from './api';
 
 console.log(config);
 
-import express from "express";
+import express from 'express';
 
 const server = express();
 
-server.get("/", (req, res) => {
-	res.send("Hello from express !");
+server.get('/', (req, res) => {
+	res.render('index', {
+		content: 'Hello Express and EJS!'
+	});
 });
 
-server.use("/api", apiRouter);
-server.use(express.static("public"));
+server.use('/api', apiRouter);
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
-	console.info("Express listening on port ", config.port);
+	console.info('Express listening on port ', config.port);
 });
