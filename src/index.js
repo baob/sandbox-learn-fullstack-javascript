881d072305cff58e592784d0a1481195a2c3780d
Andy Roberts <coder@onesandthrees.com>
2019-01-04 18:54:40 +0000

    another hack, trying (as recommended): npm install -D babel-loader @babel/core @babel/preset-env webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-	React.createElement("h2", null, "hello React"),
-	document.getElementById("root")
-);
+ReactDOM.render(<h2 className="text-center">hello react WITH JSX !!</h2>, document.getElementById('root'));
